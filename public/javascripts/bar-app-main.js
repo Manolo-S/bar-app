2ce8763df808d5cc587288d692df6afdf0cf4c1b
Manolo Schotsborg <manolo.schotsborg@outlook.com>
2016-03-23 15:06:50 -0300

    2nd commit

--- public/javascripts/bar-app-main.js
+++ public/javascripts/bar-app-main.js
@@ -17,17 +17,23 @@ $('#search-button').click(function(e){
 
 function success(results){
 	console.log(results);
+	$("#search-results").empty();
 	results.businesses.map(displayResults)
-	$(function() {
-    $('span.stars').stars();
-});
-}
+};
+
 
 function displayResults(result){
 	var div = "<div>";
-	div += "<img src=" + result.image_url +" class='barImage'>";
-	div += "<h4 class='bar-name'>" + result.name + "</h4>";
-	div += "<img src=" + result.rating_img_url + ">";
+	div += "<a href=" + result.url + " class='bar-name' target='_blank'>" + result.name + "</a>";
+	div += "<img src=" + result.rating_img_url + " class='rating-img'>";
+	div += "<p class='reviews'>Reviews: " + result.review_count + "</p>";
+	div += "<img src=" + result.image_url +" class='bar-img'>";
+	div += "<div class='text-block'>";
+	div += "<p>Address: " + result.location.address[0] + ", " + result.location.city + "</p>";
+	div += "<p>Phone: " + result.display_phone + "</p>";
+	div += '<p class="snippet">"' +  result.snippet_text + '"</p>';
+	div += "</div>";
+
 	div += "</div>";
 	$("#search-results").append(div);
 }
