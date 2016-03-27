63945d63be54a3ab81ee67e400bfc31b86875947
Manolo Schotsborg <manolo.schotsborg@outlook.com>
2016-03-27 15:45:21 -0300

    heroku deployment 2

--- routes/barsGoing.js
+++ routes/barsGoing.js
@@ -12,7 +12,9 @@ router.use('/', function(req, res, next){
 	var allbars = bars.map(function(bar){return bar.barName});
 	var addresses = bars.map(function(bar){return bar.address});
 	if (mongoose.connection.readyState === 0){
-		var db = mongoose.connect('mongodb://localhost/bar-app');
+		// var db = mongoose.connect('mongodb://localhost/bar-app');
+		var db = mongoose.connect('mongodb://piet:snot@ds025389.mlab.com:25389/local-bars')
+		
 	}
 
 	barModel.find({$and:[{barName:{$in: allbars}},{address:{$in: addresses}}]}, function(err, barData){
